398b72f6a3adc9c7d67155200117f456666ae570
sathishsundar17 <sathishsundar17@gmail.com>
2026-07-21 21:26:27 +0530

    Add files via upload

---
+++ SQLQuery6.sql
@@ -0,0 +1,11 @@
+/* distinct = removes duplicates, repeated values 
+each value appears only once  
+syntax 
+	SELECT DISTINCT 
+*/ 
+
+/* task 1. Return unique list of all countries */
+SELECT DISTINCT
+	country 
+FROM customers 
+	
